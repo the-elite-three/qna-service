e41a61a824597f4754a229ebb8e4132ad154a2a4
VincentCSS <VincentCSS@github.com>
2019-09-12 19:51:09 -0600

    testing sanitizer

--- server/index.js
+++ server/index.js
@@ -1,5 +1,13 @@
+const bodyParser = require('body-parser');
 const express = require('express');
+const expressSanitizer = require('express-sanitizer');
 const app = express();
+
+app.use(bodyParser.urlencoded({
+  extended: true,
+}));
+app.use(bodyParser.json());
+app.use(expressSanitizer())
 
 app.listen(8080, () => console.log('Listening on port 8080'));
 
